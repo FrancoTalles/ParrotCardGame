8234a0a3043dd4060362932bc8d8334437416419
FrancoTalles <francotallesextreme@gmail.com>
2022-08-21 01:57:38 -0400

    Add as funçoes de flipar e distribuir as cartas embaralhadas

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 let cartas = 0;
-iniciarJogo();
-function iniciarJogo(){
-    cartas = Number(prompt("Quantas cartas você quer jogar? 4 a 14, só pares"));
-    if (cartas < 4){
-        iniciarJogo();
-    }else if (cartas > 14){
-        iniciarJogo();
-    }else if (cartas % 2 === 0){
-        distribuirCartas();
-    }else{
-        iniciarJogo();
-    }
+const baralho = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
 
+iniciarJogo();
+function iniciarJogo() {
+  cartas = Number(prompt("Quantas cartas você quer jogar? 4 a 14, só pares"));
+  if (cartas < 4) {
+    iniciarJogo();
+  } else if (cartas > 14) {
+    iniciarJogo();
+  } else if (cartas % 2 === 0) {
+    distribuirCartas();
+  } else {
+    iniciarJogo();
+  }
 }
 
-function distribuirCartas(){
+function distribuirCartas() {
+  let embaralhadas = [];
+  for (i = 0; i < cartas; i++) {
+    embaralhadas.push(baralho[i]);
+  }
+  embaralhadas.sort(comparador);
+  console.log(embaralhadas)
+  let contador = 0;
+  while (contador < cartas) {
     const campo = document.querySelector(".jogo");
     let carta = `
-    <div class="carta">
-        <img src="imagens/front.png">
-    </div>
-    `;
-    let contador = 0;
-    while (contador < cartas){
-        campo.innerHTML += carta;
-        contador += 1;
-    }
+        <div class="container">
+            <div class="carta" onclick="flipar(this)">
+                <div class="frente">
+                    <img src="imagens/front.png" />
+                </div>
+                <div class="costa">
+                    <img src="imagens/${embaralhadas[contador]}.gif" />
+                </div>
+            </div>
+        </div>`;
+    campo.innerHTML += carta;
+    contador += 1;
+  }
+}
+
+function flipar(elemento) {
+  elemento.classList.toggle("flipcard");
+}
+
+function comparador() {
+  return Math.random() - 0.5;
 }
